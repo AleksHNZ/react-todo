dd78dbb67ec3671551555cf34a6a52043b696f92
AleksHNZ <86948893+AleksHNZ@users.noreply.github.com>
2022-02-18 11:06:27 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,201 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./css/main.css";
+import "./css/responsiveness.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import TaskLineItem from "./TaskLineItem";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ateCookies: false,
+      cookieStyle: "main-bg cookies container-flex",
+      taskCounter: 5,
+      deleteCounter: 0,
+      completedCounter: 1,
+      cookieCounter: 0,
+      tasksList: [
+        {
+          id: 0,
+          task: "Build a Project 😎🍪",
+          isDone: false,
+          focused: false
+        },
+        {
+          id: 1,
+          task: "Run 🏃‍♀",
+          isDone: false,
+          focused: false
+        },
+        {
+          id: 3,
+          task: "Make a Billie 🤑",
+          isDone: false,
+          focused: false
+        },
+        {
+          id: 1,
+          task: "Read a book",
+          isDone: false,
+          focused: false
+        },
+        {
+          id: 3,
+          task: "Portfolio website, better Done than Pefect",
+          isDone: true,
+          focused: false
+        },
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {}
+      ]
+    };
+
+    this.eachTaskItem = this.eachTaskItem.bind(this);
+    this.update = this.update.bind(this);
+    this.delete = this.delete.bind(this);
+    this.eatCookies = this.eatCookies.bind(this);
+    this.completedTasksCounter = this.completedTasksCounter.bind(this);
+    this.taskCounter = this.taskCounter.bind(this);
+    this.changeInputFocus = this.changeInputFocus.bind(this);
+  }
+  componentDidUpdate() {}
+
+  changeInputFocus(taskIndex) {
+    let tasksArray = this.state.tasksList;
+    tasksArray[taskIndex + 1].focused = true;
+    this.setState({ tasksList: tasksArray });
+  }
+  update(updatedTask, i) {
+    console.log(updatedTask, i);
+    let tasksArray = this.state.tasksList;
+    tasksArray[i].task = updatedTask;
+    this.setState({ tasksList: tasksArray });
+    this.taskCounter();
+    this.changeInputFocus(i);
+  }
+
+  delete(i) {
+    let currentTaskList = this.state.tasksList;
+    currentTaskList[i] = {};
+
+    this.setState({
+      deleteCounter: this.state.deleteCounter + 1,
+      tasksList: currentTaskList
+    });
+    this.taskCounter();
+  }
+
+  eatCookies(ateCookies) {
+    if (!this.state.ateCookies && ateCookies) {
+      this.setState({
+        ateCookies: true,
+        cookieStyle: "main-bg cookies container-flex"
+      });
+    } else {
+      this.setState({
+        cookieCounter: this.state.cookieCounter + 1,
+        ateCookies: false,
+        cookieStyle: "main-bg no-cookies container-flex"
+      });
+    }
+  }
+
+  completedTasksCounter(isChecked) {
+    if (isChecked) {
+      this.setState({ completedCounter: this.state.completedCounter + 1 });
+    } else {
+      this.setState({ completedCounter: this.state.completedCounter - 1 });
+    }
+  }
+  taskCounter() {
+    let totalTaskCounter = 0;
+
+    this.state.tasksList.forEach(function(taskObject) {
+      if (taskObject.task) {
+        totalTaskCounter += 1;
+      }
+      totalTaskCounter -= 0;
+    });
+
+    this.setState({ taskCounter: totalTaskCounter });
+  }
+
+  eachTaskItem(taskObject, i) {
+    let checkMarkStyle;
+    let textDoneStyle;
+    if (taskObject.isDone) {
+      checkMarkStyle = "checkmark complete";
+      textDoneStyle = "item-text done";
+    } else {
+      checkMarkStyle = "checkmark";
+      textDoneStyle = "item-text";
+    }
+
+    return (
+      <TaskLineItem
+        index={i}
+        key={i}
+        onUpdate={this.update}
+        onDelete={this.delete}
+        taskValue={taskObject.task}
+        eatCookies={this.eatCookies}
+        isDone={taskObject.isDone}
+        addCompletedCounter={this.completedTasksCounter}
+        checkMarkStyle={checkMarkStyle}
+        textDoneStyle={textDoneStyle}
+        totalTaskCounter={this.taskCounter}
+        focused={this.state.focused}
+      />
+    );
+  }
+  render() {
+    return (
+      <div className={this.state.cookieStyle}>
+        <span id="preload-cookies" />
+        <span id="preload-cookies-mobile" />
+
+        <div className="sticky-note">
+          <div className="row">
+            <div className="col-8">
+              <p>Current Tasks:</p>
+              <p>Completed:</p>
+              <p>Deleted:</p>
+              <p>Cookies:</p>
+            </div>
+            <div className="col">
+              <p>{this.state.taskCounter}</p>
+              <p>{this.state.completedCounter}</p>
+              <p>{this.state.deleteCounter}</p>
+              <p>{this.state.cookieCounter}</p>
+            </div>
+          </div>
+        </div>
+        <div className="paper-container row">
+          <div className="paper-left-section col-1" />
+          <div className="paper-right-section col">
+            <div className="paper-top-section">
+              <div className="paper-title">
+                <h1>A React To-Do-List</h1>
+                <h2>By @Ali</h2>
+              </div>
+            </div>
+            <div className="list-of-to-do">
+              {this.state.tasksList.map(this.eachTaskItem)}
+            </div>
+          </div>
+          <div className="paper-footer" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/TaskLineItem.js
@@ -0,0 +1,172 @@
+import React, { Component } from "react";
+import "./css/main.css";
+
+class TaskLineItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false,
+      taskDone: this.props.isDone,
+      checkMarkStyle: this.props.checkMarkStyle,
+      textDoneStyle: this.props.textDoneStyle,
+      taskInput: this.props.taskValue,
+      taskIndex: this.props.index,
+      ateCookies: false,
+      focused: this.props.focused
+    };
+
+    this.selectedInput = React.createRef();
+
+    this.edit = this.edit.bind(this);
+    this.delete = this.delete.bind(this);
+    this.save = this.save.bind(this);
+    this.editRender = this.editRender.bind(this);
+    this.initialRender = this.initialRender.bind(this);
+    this.checkMark = this.checkMark.bind(this);
+    this.inputBlur = this.inputBlur.bind(this);
+  }
+
+  componentDidUpdate() {
+    let currentInput;
+    if (this.state.taskInput && this.state.editing) {
+      currentInput = this.selectedInput.current;
+      currentInput.focus();
+    }
+  }
+
+  edit() {
+    this.setState({
+      editing: true
+    });
+  }
+
+  delete() {
+    if (this.state.taskDone) {
+      this.props.addCompletedCounter(false);
+    }
+
+    this.props.onDelete(this.state.taskIndex);
+    this.setState({
+      taskDone: false,
+      editing: false,
+      checkMarkStyle: "checkmark",
+      textDoneStyle: "item-text",
+      taskInput: ""
+    });
+  }
+
+  save(e) {
+    e.preventDefault();
+    const thisInput = this.state.taskInput;
+    if (
+      this.state.taskInput &&
+      (thisInput.includes("cookie") || thisInput.includes("galleta"))
+    ) {
+      this.setState({ ateCookies: false });
+      this.props.eatCookies(true);
+    }
+    this.props.onUpdate(this.state.taskInput, this.state.taskIndex);
+    this.setState({
+      editing: false
+    });
+  }
+
+  checkMark() {
+    if (this.state.taskDone) {
+      this.setState({
+        checkMarkStyle: "checkmark",
+        textDoneStyle: "item-text",
+        taskDone: false,
+        editing: false
+      });
+      this.props.addCompletedCounter(false);
+    } else {
+      this.setState({
+        checkMarkStyle: "checkmark complete",
+        textDoneStyle: "item-text done",
+        taskDone: true,
+        editing: false
+      });
+      this.props.addCompletedCounter(true);
+    }
+    const thisInput = this.state.taskInput.toLowerCase();
+
+    if (
+      (thisInput.includes("cookie") || thisInput.includes("galleta")) &&
+      !this.state.ateCookies
+    ) {
+      this.setState({ ateCookies: true });
+      this.props.eatCookies(false);
+    } else if (
+      (thisInput.includes("cookie") || thisInput.includes("galleta")) &&
+      this.state.ateCookies
+    ) {
+      this.setState({ ateCookies: false });
+      this.props.eatCookies(true);
+    }
+  }
+
+  inputBlur() {
+    this.save();
+  }
+
+  editRender() {
+    if (!this.state.editing) this.setState({ editing: true });
+    if (this.state.taskInput) {
+      return (
+        <div className="item-box">
+          <div className={this.state.checkMarkStyle} onClick={this.checkMark}>
+            <i className="fas fa-check" />
+          </div>
+          <form className="item-box-edit" onSubmit={this.save}>
+            <input
+              maxLength="35"
+              ref={this.selectedInput}
+              onChange={e => this.setState({ taskInput: e.target.value })}
+              value={this.state.taskInput}
+              onBlur={this.save}
+            />
+          </form>
+          <div className="btn-delete" onClick={this.delete}>
+            <i className="far fa-trash-alt" />
+          </div>
+        </div>
+      );
+    }
+    return (
+      <div className="item-box">
+        <form className="item-box-blank initial" onSubmit={this.save}>
+          <input
+            maxLength="35"
+            ref={this.selectedInput}
+            onChange={e => this.setState({ taskInput: e.target.value })}
+            value={this.state.taskInput}
+          />
+        </form>
+      </div>
+    );
+  }
+
+  initialRender() {
+    return (
+      <div className="item-box">
+        <div className={this.state.checkMarkStyle} onClick={this.checkMark}>
+          <i className="fas fa-check" />
+        </div>
+        <div className={this.state.textDoneStyle} onClick={this.edit}>
+          {this.state.taskInput}
+        </div>
+        <div className="btn-delete" onClick={this.delete}>
+          <i className="far fa-trash-alt" />
+        </div>
+      </div>
+    );
+  }
+  render() {
+    return this.state.editing || !this.state.taskInput || this.state.focused
+      ? this.editRender()
+      : this.initialRender();
+  }
+}
+
+export default TaskLineItem;
